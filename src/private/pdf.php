d976c1019ef1de41cfe4e827007749b717056ec4
Mikhail <mr1302@ya.ru>
2022-11-16 22:31:46 +0300

    PDF filter

--- src/private/pdf.php
+++ src/private/pdf.php
@@ -6,17 +6,23 @@ $dictionary = $DICTIONARY[$_SESSION['language']];
 
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    header('Location: /private/index.php');
-} else {
-    if ($_FILES['userfile']['size'] == 0){
+if ($_FILES['userfile']['type'] == 'application/pdf') {
+
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
         header('Location: /private/index.php');
+    } else {
+        if ($_FILES['userfile']['size'] == 0) {
+            header('Location: /private/index.php');
+        }
+        echo '<pre>';
+        echo $dictionary->FILE_ERROR . "\n";
     }
-    echo '<pre>';
-    echo $dictionary->FILE_ERROR . "\n";
+
+    print_r($_FILES);
+
+    print "</pre>";
 }
+else {
 
-print_r($_FILES);
-
-print "</pre>";
-?>
+    header('Location: /private/index.php');
+}
